c869545328d9f6a26fac10aebbed9e996b224443
khuzhokov <a7kerkh@yandex.ru>
2026-05-11 22:40:57 +0300

    chore: init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/askerdev/monkeylang
+
+go 1.25.8
---
+++ token/token.go
@@ -0,0 +1,65 @@
+package token
+
+type Type string
+
+const (
+	ILLEGAL Type = "ILLEGAL"
+	EOF     Type = "EOF"
+
+	// Identifiers + literals
+	IDENT Type = "IDENT"
+	INT   Type = "INT"
+
+	// Operators
+	ASSIGN   Type = "="
+	PLUS     Type = "+"
+	MINUS    Type = "-"
+	BANG     Type = "!"
+	ASTERISK Type = "*"
+	SLASH    Type = "/"
+
+	LT     = "<"
+	GT     = ">"
+	EQ     = "=="
+	NOT_EQ = "!="
+
+	// Delimeters
+	COMMA     Type = ","
+	SEMICOLON Type = ";"
+
+	LPAREN Type = "("
+	RPAREN Type = ")"
+	LBRACE Type = "{"
+	RBRACE Type = "}"
+
+	// Keywords
+	FUNCTION Type = "FUNCTION"
+	LET      Type = "LET"
+	TRUE     Type = "TRUE"
+	FALSE    Type = "FALSE"
+	IF       Type = "IF"
+	ELSE     Type = "ELSE"
+	RETURN   Type = "RETURN"
+)
+
+var keywords = map[string]Type{
+	"fn":     FUNCTION,
+	"let":    LET,
+	"true":   TRUE,
+	"false":  FALSE,
+	"if":     IF,
+	"else":   ELSE,
+	"return": RETURN,
+}
+
+type Token struct {
+	Type    Type
+	Literal string
+}
+
+func LookupIdent(ident string) Type {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
